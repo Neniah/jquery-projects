a007e76e11dd485f7d65fc7c5d75915f9a38b2be
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-25 17:45:12 +0200

    active class

---
+++ 2.content-slider/js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  // Set Options
+  var speed = 500;
+  var autoswitch = true;
+  var autoswitch_speed = 4000;
+
+  // Add initial active class
+  $('.slide').first().addClass('active');
+
+  // Hide all slides
+  $('.slide').hide();
+
+  // Shoe first slide
+  $('.active').show();
+
+  $('#next').on('click', function(){
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('oldActive').is(':last-child')){
+      $('.slide').first().addClass('active');
+    } else{
+      $('.oldActive').next().addClass('active');
+    }
+  });
+});
