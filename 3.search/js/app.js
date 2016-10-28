81aae6b111aa81b3b2ef275cd1c06bacef7a6e5f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-28 16:25:28 +0200

    search function

--- 3.search/js/app.js
+++ 3.search/js/app.js
@@ -25,4 +25,36 @@ $(function(){
     }
   });
 
+  $('#search-form').submit(function(e){
+    e.preventDefault();
+  });
+
 })
+
+
+function search(){
+  // clear results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  // Get Form Input
+  q = $('#query').val(),
+
+  // Run GET Request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search", {
+        part: 'snippet, id',
+        q: q,
+        type: 'video',
+        key: '6091ff2dab113c6256971ecaba24302485001db8'
+    }, function(data){
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      console.log(data);
+
+      
+
+    }
+  );
+}
