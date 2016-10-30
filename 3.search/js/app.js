620f07194ddfece3ce8554bf98b8e1660c1c9793
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-30 10:45:02 +0100

    js files updating

--- 3.search/js/app.js
+++ 3.search/js/app.js
@@ -1,94 +1,112 @@
 // Searchbar Handler
 $(function(){
-  var searchField = $('#query');
-  var icon = $('#search-btn');
-
-  //Focus Handler
-  $(searchField).on('focus', function(){
-    $(this).animate({
-      width: '100%'
-    }, 400);
-    $(icon).animate({
-      right: '10px'
-    }, 400);
-  });
-
-  // Blur Ever Handler
-  $(searchField).on('blur', function(){
-    if(searchField.val() == ''){
-      $(searchField).animate({
-        width:'45%'
-      },400, function(){});
-      $(icon).animate({
-        right:'360px'
-      },400, function(){});
-    }
-  });
-
-  $('#search-form').submit(function(e){
-    e.preventDefault();
-  });
-
+	var searchField = $('#query');
+	var icon = $('#search-btn');
+	
+	// Focus Event Handler
+	$(searchField).on('focus', function(){
+		$(this).animate({
+			width:'100%'
+		},400);
+		$(icon).animate({
+			right: '10px'
+		}, 400);
+	});
+	
+	// Blur Event Handler
+	$(searchField).on('blur', function(){
+		if(searchField.val() == ''){
+			$(searchField).animate({
+				width:'45%'
+			},400, function(){});
+			$(icon).animate({
+				right:'360px'
+			},400, function(){});
+		}
+	});
+	
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+	});
 })
 
 
 function search(){
-  // clear results
-  $('#results').html('');
-  $('#buttons').html('');
-
-  // Get Form Input
-  q = $('#query').val(),
-
-  // Run GET Request on API
-  $.get(
-    "https://www.googleapis.com/youtube/v3/search", {
-        part: 'snippet, id',
-        q: q,
-        type: 'video',
-        key: '6091ff2dab113c6256971ecaba24302485001db8'
-    }, function(data){
-      var nextPageToken = data.nextPageToken;
-      var prevPageToken = data.prevPageToken;
-
-      console.log(data);
-
-      $.each(data.items, function(i, item){
-        // Get Output
-        var output = getOutput(item);
-
-        // Display Results
-        $('#results').append(output);
-      });
-
-    }
-  );
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			type:'video',
+			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
 }
 
-
 // Build Output
-function getOutput(){
-  var videoId = item.id.videoId;
-  var title = item.snipped.title;
-  var description = item.snipped.description;
-  var thumb = item.snipped.thumbnails.high.url;
-  var channelTitle = item.snippet.channelTitle;
-  var videoDate = item.snipped.publishedAt;
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+	
+	// Build Output String
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3>'+title+'</h3>' +
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+	
+	return output;
+}
 
-  // Build OutPut String
-  var output = '<li>' +
-  '<div class="list-left">' +
-  '<img src="'+ thumb + '">' +
-  '</div>' +
-  '<div class="list-right">' +
-  '<h3>'+ title +'<h3/>' +
-  '<small>By <span class="cTitle">'+ channelTitle +'</span></small>'+
-  '<p>'+ description +'</p>'+
-  '</div>'+
-  '</li>'+
-  '<div class="clearfix"></div>' +
-  '';
-
-  return output;
-
+// Build the buttons
+function getButtons(prevPageToken, nextPageToken){
+	if(!prevPageToken){
+		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">'+
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'onclick="prevPage();">Prev Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	}
+	
+	return btnoutput;
 }
