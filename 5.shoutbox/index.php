40f4c8c26dea601807e3047cd381e77dc8a568c6
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-11-02 10:30:25 +0100

    to connect to database

--- 5.shoutbox/index.php
+++ 5.shoutbox/index.php
@@ -1,5 +1,10 @@
 <?php
-include 'database.php';
+//Connect to Mysql
+$connection = mysql_connect('localhost', 'root', '', 'shotbox');
+
+if(mysqli_connect_errno()){
+  echo 'Failed to Connect' . mysqli_connect_error();
+}
 ?>
 
 <!DOCTYPE html>
