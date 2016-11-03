8aab5d538b96eac908907bbbb14b9e47f629ef50
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-11-03 15:47:05 +0100

    ajax

--- 5.shoutbox/index.php
+++ 5.shoutbox/index.php
@@ -2,7 +2,6 @@
 //Connect to Mysql
 include('database.php');
 ?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,7 +9,7 @@ include('database.php');
     <title>Shout Box</title>
     <link rel="stylesheet" href="css/style.css" media="screen" title="no title">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script type="text/javascript" scr="js/app.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
   </head>
   <body>
     <div class="container">
@@ -24,7 +23,7 @@ include('database.php');
       </div>
 
       <footer>
-        <form class="form" action="" method="post" >
+        <form class="form" action="" method="" >
           <label for="name">Name:</label>
           <input type="text" id="name" value="">
           <br>
--- 5.shoutbox/js/app.js
+++ 5.shoutbox/js/app.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
-  $('submit').on('click', function(){
+  $('#submit').on('click', function(){
     var name = $('#name').val();
     var shout = $('#shout').val();
-    var date = getDate();
-    var dataString = 'name' + name + '&shout=' + '&date=' + date;
+    //var date = getDate();
+    //var dataString = 'name' + name + '&shout=' + '&date=' + date;
 
     // Validation
     if (name == '' || shout == ''){
       alert('Please fill in your name and shout');
+    } else{
+      $.ajax({
+        type: 'POST',
+        url: '../5.shoutbox/shoutbox.php',
+        data: dataString,
+        cache: false,
+        success:function(html){
+          $('#shouts ul').prepend(html);
+        }
+      });
     }
 
     return false;
