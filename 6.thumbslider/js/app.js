b2dea2372d4c3c7243d785913118860ada3836d9
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-11-14 19:48:55 +0100

    js for slides

--- 6.thumbslider/js/app.js
+++ 6.thumbslider/js/app.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
   var totalWidth = 0;
-  var positions = new Aray();
+  var positions = new Array();
+
+  $('#slide slide').each(function(i){
+    positions[i] = totalWidth;
+    totalWidth += $(this).width();
+
+    if(!$(this).width()){
+      alert('Please add a width to your images');
+      return false;
+    }
+  });
+
+  $('#slides').width(totalWidth);
+
+  $('#menu ul li a').click(function(e, keepScroll){
+    $('li.products').removeClass('active').addClass('inactive');
+    //
+  });
 });
